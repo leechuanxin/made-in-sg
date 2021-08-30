5ea7f6525e4fb6fbb10226e3b352780f6eb9edb7
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-31 00:33:21 +0800

    feat: tables.

---
+++ init_tables.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS stories;
+DROP TABLE IF EXISTS starting_paragraphs;
+DROP TABLE IF EXISTS ending_paragraphs;
+DROP TABLE IF EXISTS paragraphs;
+DROP TABLE IF EXISTS paragraphs_keywords;
+DROP TABLE IF EXISTS keywords;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT
+);
+
+CREATE TABLE IF NOT EXISTS stories (
+  id SERIAL PRIMARY KEY,
+  created_user_id INTEGER,
+  last_updated_user_id INTEGER,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  last_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  starting_paragraph_id INTEGER,
+  ending_paragraph_id INTEGER,
+  title TEXT
+);
+
+CREATE TABLE IF NOT EXISTS starting_paragraphs (
+  id SERIAL PRIMARY KEY,
+  paragraph TEXT
+);
+
+CREATE TABLE IF NOT EXISTS ending_paragraphs (
+  id SERIAL PRIMARY KEY,
+  paragraph TEXT
+);
+
+CREATE TABLE IF NOT EXISTS paragraphs (
+  id SERIAL PRIMARY KEY,
+  created_user_id INTEGER,
+  last_updated_user_id INTEGER,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  last_updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  story_id INTEGER,
+  paragraph TEXT
+);
+
+CREATE TABLE IF NOT EXISTS paragraphs_keywords (
+  id SERIAL PRIMARY KEY,
+  paragraph_id INTEGER,
+  keyword_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS keywords (
+  id SERIAL PRIMARY KEY,
+  keyword TEXT,
+  type TEXT
+);
