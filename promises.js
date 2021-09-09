94bc7a2194911837eff20697712f63d11d31e9e6
Lee Chuan Xin <chuanxin@gametize.com>
2021-09-10 04:06:34 +0800

    refactor: promise callbacks return function instead of promises.

--- promises.js
+++ promises.js
@@ -114,10 +114,31 @@ const getKeywordsText = (pool) => (result) => new Promise((resolve, reject) => {
     });
 });
 
-const getAllParagraphs = (pool) => (result) => new Promise((resolve, reject) => {
-  const paragraphsQuery = `SELECT paragraphs.id, paragraphs.created_user_id, users.realname AS created_username, paragraphs.last_updated_user_id, paragraphs.story_id, paragraphs.paragraph FROM paragraphs INNER JOIN users ON users.id=paragraphs.created_user_id WHERE story_id=${result.id} ORDER BY id ASC`;
-  pool
-    .query(paragraphsQuery)
+// const getAllParagraphs = (pool) => (result) => new Promise((resolve, reject) => {
+//   const paragraphsQuery = `SELECT paragraphs.id, paragraphs.created_user_id, users.realname AS created_username, paragraphs.last_updated_user_id, paragraphs.story_id, paragraphs.paragraph FROM paragraphs INNER JOIN users ON users.id=paragraphs.created_user_id WHERE story_id=${result.id} ORDER BY id ASC`;
+//   pool
+//     .query(paragraphsQuery)
+//     .then((paragraphsQueryResult) => {
+//       const paragraphs = (paragraphsQueryResult.rows.length > 0) ? paragraphsQueryResult.rows : [];
+//       const paragraphsFmt = paragraphs.map((paragraph) => ({
+//         ...paragraph,
+//         created_username_fmt: paragraph.created_username,
+//       }));
+//       const newResult = {
+//         ...result,
+//         paragraphs: paragraphsFmt,
+//       };
+//       resolve(newResult);
+//     })
+//     .catch((error) => {
+//       reject(new Error(error.message));
+//     });
+// });
+
+const getAllParagraphs = (pool) => (story) => {
+  const paragraphsQuery = `SELECT paragraphs.id, paragraphs.created_user_id, users.realname AS created_username, paragraphs.last_updated_user_id, paragraphs.story_id, paragraphs.paragraph FROM paragraphs INNER JOIN users ON users.id=paragraphs.created_user_id WHERE story_id=${story.id} ORDER BY id ASC`;
+
+  return pool.query(paragraphsQuery)
     .then((paragraphsQueryResult) => {
       const paragraphs = (paragraphsQueryResult.rows.length > 0) ? paragraphsQueryResult.rows : [];
       const paragraphsFmt = paragraphs.map((paragraph) => ({
@@ -125,17 +146,37 @@ const getAllParagraphs = (pool) => (result) => new Promise((resolve, reject) => 
         created_username_fmt: paragraph.created_username,
       }));
       const newResult = {
-        ...result,
+        ...story,
         paragraphs: paragraphsFmt,
       };
-      resolve(newResult);
-    })
-    .catch((error) => {
-      reject(new Error(error.message));
+      return newResult;
     });
-});
+};
+
+// new Promise((resolve, reject) => {
+//   const paragraphsQuery = `SELECT paragraphs.id, paragraphs.created_user_id, users.realname AS created_username, paragraphs.last_updated_user_id, paragraphs.story_id, paragraphs.paragraph FROM paragraphs INNER JOIN users ON users.id=paragraphs.created_user_id WHERE story_id=${result.id} ORDER BY id ASC`;
+//   pool
+//     .query(paragraphsQuery)
+//     .then((paragraphsQueryResult) => {
+//       const paragraphs = (paragraphsQueryResult.rows.length > 0) ? paragraphsQueryResult.rows : [];
+//       const paragraphsFmt = paragraphs.map((paragraph) => ({
+//         ...paragraph,
+//         created_username_fmt: paragraph.created_username,
+//       }));
+//       const newResult = {
+//         ...result,
+//         paragraphs: paragraphsFmt,
+//       };
+//       resolve(newResult);
+//     })
+//     .catch((error) => {
+//       reject(new Error(error.message));
+//     });
+// });
 
 const getOrSetStoryCollab = (pool, request) => new Promise((resolve, reject) => {
+  // think of a way to ensure that each function can be modularised further,
+  // so that they can be reused instead of being stuck in this function
   getStory(pool, request)
     .then(getCollabInStory(pool, request))
     .then(getAllKeywords(pool))
